1d705b79ce2d61add35a2d427bff110e040b485e
Chiunhau <chiunhau9@gmail.com>
2015-11-19 00:20:54 +0800

    pray for paris 2

--- prayForParis2.js
+++ prayForParis2.js
@@ -6,8 +6,10 @@ function setup() {
 
 function draw() {
 	noStroke();
+	background(0);
 	for (var i = 0; i < 9; i ++) {
 		for (var j = 0; j < 9; j ++) {
+		  var sinSeed = sin((frameCount + (i * j) * 5) * 0.05) + 1;
 			if ( i <= 2 ) {
 				fill(color("blue"));
 			}
@@ -17,7 +19,7 @@ function draw() {
 			else {
 				fill(color("red"));
 			}
-			ellipse(i * 40, j * 40, 30, 30);
+			ellipse(i * 40, j * 40, sinSeed * 10 + 5, sinSeed * 10 + 5);
 		}
 	}
 }
